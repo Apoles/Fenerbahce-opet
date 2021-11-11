9479ce8d93eaf22d9a0258f5239d617e0498c13f
Apoles <Emine176gumus@gmail.com>
2021-11-11 14:37:50 +0300

    first commit

---
+++ pages/dens.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyDen from "../components/den";
+import Example from "../components/navbar";
+
+export default function dens() {
+  return (
+    <div>
+      <MyDen></MyDen>
+    </div>
+  );
+}
